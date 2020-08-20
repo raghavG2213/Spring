e51561933cf6a6dcd83127cd326a6fe60a7460c9
raghavG2213 <68116350+raghavG2213@users.noreply.github.com>
2020-08-20 09:07:11 +0530

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
   @RequestMapping("/")
   public String home() {
-    return "Bruhhhhhhhh!";
+    return "Hello WOrld! This is a Spring Application";
   }
 
   public static void main(String[] args) {
